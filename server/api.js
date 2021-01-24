6dca47822deabd3214f59776c010cdf6f513c8f4
Taylor Shaw <tay@taylorcodeshop.com>
2021-01-24 01:16:10 -0500

    wrote up whole board

--- server/api.js
+++ server/api.js
@@ -13,6 +13,7 @@ const express = require("express");
 const User = require("./models/user");
 const Player = require("./models/player");
 const Space = require("./models/space");
+const staticSpaces = require("./staticSpaces");
 
 // import authentication library
 const auth = require("./auth");
@@ -45,128 +46,12 @@ router.post("/initsocket", (req, res) => {
 // | write your API methods below!|
 // |------------------------------|
 
-const board = {
-  spaces: [
-    {
-      _id: 0,
-      name: "GO",
-      canOwn: false,
-    }, {
-      _id: 1,
-      name: "Edgerton House",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 50,
-      rentPerBooth: 5,
-      numberOfBooths: 0,
-      color: "Purple",
-    }, {
-      _id: 2,
-      name: "WILG",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 50,
-      rentPerBooth: 5,
-      numberOfBooths: 0,
-      color: "Purple",
-    }, {
-      _id: 3,
-      name: "Next House",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 75,
-      rentPerBooth: 10,
-      numberOfBooths: 0,
-      color: "LightBlue",
-    }, {
-      _id: 4,
-      name: "New House",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 75,
-      rentPerBooth: 10,
-      numberOfBooths: 0,
-      color: "LightBlue",
-    }, {
-      _id: 5,
-      name: "Simmons",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 75,
-      rentPerBooth: 10,
-      numberOfBooths: 0,
-      color: "DeepPink",
-    }, {
-      _id: 6,
-      name: "Maseeh Hall",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 75,
-      rentPerBooth: 10,
-      numberOfBooths: 0,
-      color: "DeepPink",
-    }, {
-      _id: 7,
-      name: "Student Center",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 100,
-      rentPerBooth: 10,
-      numberOfBooths: 0,
-      color: "DarkOrange",
-    }, {
-      _id: 8,
-      name: "Lobby 7",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 110,
-      rentPerBooth: 10,
-      numberOfBooths: 0,
-      color: "red",
-    }, {
-      _id: 9,
-      name: "Building 5",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 110,
-      rentPerBooth: 10,
-      numberOfBooths: 0,
-      color: "red",
-    }, {
-      _id: 10,
-      name: "Killian Court",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 200,
-      rentPerBooth: 20,
-      numberOfBooths: 0,
-      color: "Gold",
-    }, {
-      _id: 11,
-      name: "Senior House",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 125,
-      rentPerBooth: 15,
-      numberOfBooths: 0,
-      color: "green",
-    }, {
-      _id: 12,
-      name: "Lobby 10",
-      canOwn: true,
-      owner: "MIT",
-      pricePerBooth: 150,
-      rentPerBooth: 15,
-      numberOfBooths: 0,
-      color: "blue",
-    }
-  ]
-};
+const board = staticSpaces.board;
 
 router.get("/board", (req, res) => {
   Space.find({}).then((dBSpaces) => {
     board.spaces.forEach((s) => {
-      const dBSpace = dBSpaces.find((dBs) => dBs.name === s.name);
+      const dBSpace = dBSpaces.find((dBs) => dBs._id === s._id);
       if (dBSpace) {
         s.owner = dBSpace.owner;
         s.numberOfBooths = dBSpace.numberOfBooths;
